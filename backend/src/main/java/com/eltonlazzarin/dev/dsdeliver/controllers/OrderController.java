c38c4b6f7f1258c214d351257aea354b6894e288
Elton Lazzarin <elton.lazzarin@outlook.com>
2021-01-07 18:43:49 -0300

    created endpoint post /orders

--- backend/src/main/java/com/eltonlazzarin/dev/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/eltonlazzarin/dev/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.eltonlazzarin.dev.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eltonlazzarin.dev.dsdeliver.dto.OrderDTO;
 import com.eltonlazzarin.dev.dsdeliver.services.OrderService;
@@ -22,5 +26,13 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll() {
 		List<OrderDTO> list = service.finAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/src/main/java/com/eltonlazzarin/dev/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/eltonlazzarin/dev/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.eltonlazzarin.dev.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eltonlazzarin.dev.dsdeliver.dto.OrderDTO;
+import com.eltonlazzarin.dev.dsdeliver.dto.ProductDTO;
 import com.eltonlazzarin.dev.dsdeliver.entities.Order;
+import com.eltonlazzarin.dev.dsdeliver.entities.OrderStatus;
+import com.eltonlazzarin.dev.dsdeliver.entities.Product;
 import com.eltonlazzarin.dev.dsdeliver.repositories.OrderRepository;
+import com.eltonlazzarin.dev.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,25 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> finAll() {
 		List<Order> list = repository.findOrderWithProducts();
 		return list.stream().map(item -> new OrderDTO(item)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		
+		for (ProductDTO prod : dto.getProducts()) {
+			Product product = productRepository.getOne(prod.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
