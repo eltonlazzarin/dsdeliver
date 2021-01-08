856110fb28e295b22d65b4fbd7469f40a04cd946
Elton Lazzarin <elton.lazzarin@outlook.com>
2021-01-08 12:26:35 -0300

    list products from api and format price

---
+++ frontend/src/components/ProductsList/index.tsx
@@ -0,0 +1,21 @@
+import { Product } from '../../pages/Orders';
+
+import ProductCard from '../ProductCard';
+
+interface Props {
+  products: Product[];
+}
+
+function ProductsList({ products }: Props) {
+  return (
+    <div className="orders-list-container">
+      <div className="orders-list-items">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductsList;
--- frontend/src/pages/Orders/index.tsx
+++ frontend/src/pages/Orders/index.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react';
+
+import ProductsList from '../../components/ProductsList';
 import StepsHeader from '../../components/StepsHeader';
+
+import api from '../../services/api';
+
 import './styles.css';
 
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imageUri: string;
+}
+
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    api
+      .get('products')
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProductsList products={products} />
     </div>
   );
 }
