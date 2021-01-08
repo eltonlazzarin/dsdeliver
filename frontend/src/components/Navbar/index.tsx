2c211d54a46364ab8f1464b58e7ce627230e6dad
Elton Lazzarin <elton.lazzarin@outlook.com>
2021-01-08 09:36:06 -0300

    add navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+function Navbar() {
+  return (
+    <nav className="main-navbar">
+      <Logo />
+      <a className="logo-text" href="/">
+        DS Delivery
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
