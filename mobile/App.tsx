a9371a7f4d790abbd8cd3df6aea04919f3483938
Elton Lazzarin <elton.lazzarin@outlook.com>
2021-01-09 21:00:13 -0300

    add home page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {
+  useFonts,
+  OpenSans_400Regular,
+  OpenSans_700Bold,
+} from '@expo-google-fonts/open-sans';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './src/components/Header';
+import Home from './src/pages/Home';
+
+import AppLoading from 'expo-app-loading';
+
 export default function App() {
+  let [fontsLoaded] = useFonts({ OpenSans_400Regular, OpenSans_700Bold });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header />
+      <Home />
     </View>
   );
 }
@@ -14,8 +31,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
